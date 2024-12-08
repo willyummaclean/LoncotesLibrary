3fdaad489478ab3ebf4dcb6be4a7ddf95cb75096
Will MacLean <maclean.will@gmail.com>
2024-12-07 20:07:57 -0600

    migrations

--- LoncotesLibraryDbContext.cs
+++ LoncotesLibraryDbContext.cs
@@ -1,7 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using Library.Models;
 
-public class LoncotesLibaryDbContext : DbContext
+public class LoncotesLibraryDbContext : DbContext
 {
 
     public DbSet<Genre> Genres { get; set; }
@@ -10,8 +10,51 @@ public class LoncotesLibaryDbContext : DbContext
     public DbSet<Material> Materials { get; set; }
     public DbSet<Checkout> Checkouts { get; set; }
 
-    public LoncotesLibaryDbContext(DbContextOptions<LoncotesLibaryDbContext> context) : base(context)
+    public LoncotesLibraryDbContext(DbContextOptions<LoncotesLibraryDbContext> context) : base(context)
     {
+
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        // seed data with campsite types
+        modelBuilder.Entity<Genre>().HasData(new Genre[]
+        {
+        new Genre {Id = 1, Name = "Horror"},
+        new Genre {Id = 2, Name = "Non-Fiction"},
+        new Genre {Id = 3, Name = "Music"},
+        new Genre {Id = 4, Name = "Silly Putty"}
+        });
+
+        modelBuilder.Entity<MaterialType>().HasData(new MaterialType[]
+        {
+        new MaterialType {Id = 1, Name = "Book", DaysCheckedOut = 6},
+        new MaterialType {Id = 2, Name = "DVD", DaysCheckedOut = 10},
+        new MaterialType {Id = 3, Name = "Micofiche", DaysCheckedOut = 3},
+        new MaterialType {Id = 4, Name = "Putty", DaysCheckedOut = 21}
+        });
+
+        modelBuilder.Entity<Material>().HasData(new Material[]
+        {
+        new Material {Id = 1, MaterialName = "Dracula", MaterialTypeId = 1, GenreId = 1},
+        new Material {Id = 2, MaterialName = "E.T", MaterialTypeId = 2, GenreId = 2},
+        new Material {Id = 3, MaterialName = "Silly Putty", MaterialTypeId = 4, GenreId = 4},
+        new Material {Id = 4, MaterialName = "Paranoid", MaterialTypeId = 3, GenreId = 3}
+        });
+
+        modelBuilder.Entity<Patron>().HasData(new Patron[]
+        {
+        new Patron {Id = 1, FirstName = "Horror", LastName = "Story", Address = "Cave in the Woods", Email = "patron1@email.com", IsActive = true },
+        new Patron {Id = 2, FirstName = "Gloria", LastName = "Hood", Address = "Cabin in the Woods", Email = "patron2@email.com", IsActive = true },
+        new Patron {Id = 3, FirstName = "Steve", LastName = "Tub", Address = "Pond in the Woods", Email = "patron3@email.com", IsActive = false }
+        });
+
+        modelBuilder.Entity<Checkout>().HasData(new Checkout[]
+        {
+        new Checkout {Id = 1, MaterialId = 1, PatronId = 1, CheckedOutSince = new DateTime(12/07/2024) },
+        new Checkout {Id = 2, MaterialId = 2, PatronId = 2, CheckedOutSince = new DateTime(12/06/2024)},
+        new Checkout {Id = 3, MaterialId = 3, PatronId = 3, CheckedOutSince = new DateTime(12/05/2024) }
+        });
 
     }
 }
---
+++ Migrations/20241208020635_InitialCreate.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace LoncotesLibrary.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Checkouts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    MaterialId = table.Column<int>(type: "integer", nullable: false),
+                    PatronId = table.Column<int>(type: "integer", nullable: false),
+                    CheckedOutSince = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    ReturnDate = table.Column<DateTime>(type: "timestamp without time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Checkouts", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Genres",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Genres", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Materials",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    MaterialName = table.Column<string>(type: "text", nullable: false),
+                    MaterialTypeId = table.Column<int>(type: "integer", nullable: false),
+                    GenreId = table.Column<int>(type: "integer", nullable: false),
+                    OutOfCirculationSince = table.Column<DateTime>(type: "timestamp without time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Materials", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "MaterialTypes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    DaysCheckedOut = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MaterialTypes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Patrons",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    FirstName = table.Column<string>(type: "text", nullable: false),
+                    LastName = table.Column<string>(type: "text", nullable: false),
+                    Address = table.Column<string>(type: "text", nullable: false),
+                    IsActive = table.Column<bool>(type: "boolean", nullable: false),
+                    Email = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Patrons", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Checkouts",
+                columns: new[] { "Id", "CheckedOutSince", "MaterialId", "PatronId", "ReturnDate" },
+                values: new object[,]
+                {
+                    { 1, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), 1, 1, null },
+                    { 2, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), 2, 2, null },
+                    { 3, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), 3, 3, null }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Genres",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Horror" },
+                    { 2, "Non-Fiction" },
+                    { 3, "Music" },
+                    { 4, "Silly Putty" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "MaterialTypes",
+                columns: new[] { "Id", "DaysCheckedOut", "Name" },
+                values: new object[,]
+                {
+                    { 1, 6, "Book" },
+                    { 2, 10, "DVD" },
+                    { 3, 3, "Micofiche" },
+                    { 4, 21, "Putty" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Materials",
+                columns: new[] { "Id", "GenreId", "MaterialName", "MaterialTypeId", "OutOfCirculationSince" },
+                values: new object[,]
+                {
+                    { 1, 1, "Dracula", 1, null },
+                    { 2, 2, "E.T", 2, null },
+                    { 3, 4, "Silly Putty", 4, null },
+                    { 4, 3, "Paranoid", 3, null }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Patrons",
+                columns: new[] { "Id", "Address", "Email", "FirstName", "IsActive", "LastName" },
+                values: new object[,]
+                {
+                    { 1, "Cave in the Woods", "patron1@email.com", "Horror", true, "Story" },
+                    { 2, "Cabin in the Woods", "patron2@email.com", "Gloria", true, "Hood" },
+                    { 3, "Pond in the Woods", "patron3@email.com", "Steve", false, "Tub" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Checkouts");
+
+            migrationBuilder.DropTable(
+                name: "Genres");
+
+            migrationBuilder.DropTable(
+                name: "Materials");
+
+            migrationBuilder.DropTable(
+                name: "MaterialTypes");
+
+            migrationBuilder.DropTable(
+                name: "Patrons");
+        }
+    }
+}
